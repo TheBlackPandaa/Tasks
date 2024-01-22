3f0e111b5a7cb0bed0571d36c375767827185d1f
Andrzej Maliszewski <adnrzej@netmb.pl>
2024-01-22 23:05:10 +0100

    23.4 Zadanie: tworzenie schedulera

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@EnableScheduling
 @OpenAPIDefinition
 @SpringBootApplication
 public class TasksApplication {
